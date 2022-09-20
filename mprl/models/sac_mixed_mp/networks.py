1aafc4dcc96212161d64719a30d33da83a2a6f92
freiberg-roman <roman.sascha@googlemail.com>
2022-09-20 16:25:29 +0200

    unify networks

--- mprl/models/sac_mixed_mp/networks.py
+++ mprl/models/sac_mixed_mp/networks.py
@@ -12,27 +12,31 @@ LOG_SIG_MIN = -20
 epsilon = 1e-6
 
 
-class GaussianMotionPrimitivePolicy(nn.Module):
+class GaussianPolicyWeights(nn.Module):
     def __init__(
         self,
-        num_inputs: int,
-        num_weights: int,
-        hidden_dim: int,
+        input_dim: Tuple[int, int],
+        network_width: int,
+        network_depth: int,
     ):
-        super(GaussianMotionPrimitivePolicy, self).__init__()
-        self.num_weights: int = num_weights
+        super(GaussianPolicyWeights, self).__init__()
 
-        self.linear1: nn.Linear = nn.Linear(num_inputs, hidden_dim)
-        self.linear2: nn.Linear = nn.Linear(hidden_dim, hidden_dim)
-        self.mean_linear: nn.Linear = nn.Linear(hidden_dim, num_weights)
+        state_dim, weights_dim = input_dim
 
-        self.log_std_linear: nn.Linear = nn.Linear(hidden_dim, num_weights)
+        self.linear_input = nn.Linear(state_dim, network_width)
+        self.pipeline = []
+        for i in range(network_depth - 1):
+            self.pipeline.append(nn.Linear(network_width, network_width))
+        self.pipeline = nn.ModuleList(self.pipeline)
+        self.mean_linear: nn.Linear = nn.Linear(network_width, weights_dim)
+        self.log_std_linear: nn.Linear = nn.Linear(network_width, weights_dim)
 
         self.apply(weights_init_)
 
     def forward(self, state: torch.Tensor) -> Tuple[torch.Tensor, torch.Tensor]:
-        x = F.silu(self.linear1(state))
-        x = F.silu(self.linear2(x))
+        x = F.silu(self.linear_input(state))
+        for l in self.pipeline:
+            x = F.silu(l(x))
         mean = self.mean_linear(x)
         log_std = self.log_std_linear(x)
         log_std = torch.clamp(log_std, min=LOG_SIG_MIN, max=LOG_SIG_MAX)
@@ -51,5 +55,5 @@ class GaussianMotionPrimitivePolicy(nn.Module):
 
         return weights, log_prob.unsqueeze(dim=-1), mean
 
-    def to(self, device: torch.device) -> "GaussianMotionPrimitivePolicy":
-        return super(GaussianMotionPrimitivePolicy, self).to(device)
+    def to(self, device: torch.device) -> "GaussianPolicyWeights":
+        return super(GaussianPolicyWeights, self).to(device)
