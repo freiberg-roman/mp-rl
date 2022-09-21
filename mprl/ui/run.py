69366f52740d74142083fa15178be5fa7fde5c14
freiberg-roman <roman.sascha@googlemail.com>
2022-09-21 14:38:03 +0200

    remove commented out code

--- mprl/ui/run.py
+++ mprl/ui/run.py
@@ -25,25 +25,6 @@ def run(cfg: DictConfig):
     else:
         raise ValueError(f"Unknown algorithm {cfg.alg.name}")
 
-    # if cfg.algorithm.name == "sac":
-    #     train_sac(cfg.algorithm, cfg.env, cfg.logger)
-
-    # if cfg.algorithm.name == "mp_sac":
-    #     train_mp_sac_vanilla(
-    #         cfg.algorithm, cfg.env, cfg.logger
-    #     )  # inefficient version as reference
-
-    # if cfg.algorithm.name == "sac_mp_stepwise":
-    #     if cfg.algorithm.prediction.name == "OffPolicy":
-    #         train_stepwise_mp_sac_offpolicy(cfg.algorithm, cfg.env, cfg.logger)
-    #     elif (
-    #         cfg.algorithm.prediction.name == "GroundTruth"
-    #         or cfg.algorithm.prediction.name == "MixtureOfExperts"
-    #     ):
-    #         train_stepwise_mp_sac(cfg.algorithm, cfg.env, cfg.logger)
-    #     else:
-    #         raise ValueError("Unknown prediction type: cfg.algorithm.prediction")
-
     # Main loop
     training_environment = MujocoFactory(env_config_gateway=config_repository).create()
     evaluation_environment = MujocoFactory(
