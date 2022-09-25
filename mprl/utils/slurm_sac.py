14f990cee37833afd7091b328c16e1fd04844bdb
freiberg-roman <roman.sascha@googlemail.com>
2022-09-25 17:32:02 +0200

    ready slurm

---
+++ mprl/utils/slurm_sac.py
@@ -0,0 +1,31 @@
+import os
+
+
+def run():
+
+    for env_name in [
+        "half_cheetah",
+        "meta_pd_reacher",
+        "meta_pos_reacher",
+        "meta_pd_window_open",
+        "meta_pos_window_open",
+        "meta_pd_button_press",
+        "meta_pos_button_press",
+    ]:
+        for i in range(5):
+            launch_command = "python -m mprl.ui.run alg=sac " "env={} run_id={}".format(
+                env_name,
+                i,
+            )
+            file_content = "#!/bin/bash\n" + launch_command
+            file_name = "sac_{}_{}.sh".format(env_name, i)
+            with open(file_name, "w") as text_file:
+                text_file.write(file_content)
+            os.system("chmod +x {}".format(file_name))
+            os.system(
+                "sbatch -p single -N 1 -t 72:00:00 --mem=8000 {}".format(file_name)
+            )
+
+
+if __name__ == "__main__":
+    run()
