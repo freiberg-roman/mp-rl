3c6455408c3e8b72eb6587948d265c66df0876b9
freiberg-roman <roman.sascha@googlemail.com>
2022-10-29 13:27:33 +0200

    repair net

--- mprl/models/physics/moe.py
+++ mprl/models/physics/moe.py
@@ -8,39 +8,42 @@ from torch.distributions.independent import Independent
 from torch.distributions.mixture_same_family import MixtureSameFamily
 from torch.distributions.multivariate_normal import MultivariateNormal
 
-from mprl.models import Predictable
 from mprl.utils.ds_helper import to_ts
 
 
-class MixtureOfExperts(nn.Module, Predictable):
+class MixtureOfExperts(nn.Module):
     def __init__(
         self,
-        cfg,
-        prep_input_fn=lambda x: x,
+        state_dim_in,
+        action_dim,
+        state_dim_out,
+        num_experts,
+        network_width,
+        variance=1.0,
     ):
         super(MixtureOfExperts, self).__init__()
 
-        self.bn_in = nn.BatchNorm1d(cfg.env.state_dim)
-        self.bb_out = nn.BatchNorm1d(cfg.env.state_dim)
-        self._prep_input_fn = prep_input_fn
+        self.bn_in = nn.BatchNorm1d(state_dim_in)
+        self.bb_out = nn.BatchNorm1d(state_dim_out)
         self.linear1 = nn.Linear(
-            cfg.env.state_dim + cfg.env.action_dim,
-            cfg.hidden_dim,
+            state_dim_in + action_dim,
+            network_width,
         )
-        self.linear2 = nn.Linear(cfg.hidden_dim, cfg.hidden_dim)
-        self.expert_log_prob_contribution = nn.Linear(cfg.hidden_dim, cfg.num_experts)
+        self.linear2 = nn.Linear(network_width, network_width)
+        self.expert_log_prob_contribution = nn.Linear(network_width, num_experts)
         self.expert_heads = []
 
-        for _ in range(cfg.num_experts):
-            self.expert_heads.append(nn.Linear(cfg.hidden_dim, cfg.env.state_dim))
+        for _ in range(num_experts):
+            self.expert_heads.append(nn.Linear(network_width, state_dim_out))
 
         self.expert_heads = nn.ModuleList(self.expert_heads)
-        self._action_dim = cfg.env.action_dim
+        self._action_dim = action_dim
         self.optimizer: torch.optim.Optimizer = torch.optim.Adam(self.parameters())
+        self.variance = variance
 
     def forward(self, state, action):
         n_state = self.bn_in(self._prep_input_fn(state))
-        net_in = torch.cat([n_state, action], 1)
+        net_in = torch.cat([n_state, action], dim=-1)
 
         x1 = F.silu(self.linear1(net_in))
         x2 = F.silu(self.linear2(x1))
@@ -54,7 +57,7 @@ class MixtureOfExperts(nn.Module, Predictable):
         means = torch.stack(means, 1)
 
         ind_expert_dist = Independent(
-            MultivariateNormal(means, torch.eye(means.size(dim=-1))), 0
+            MultivariateNormal(means, self.variance * torch.eye(means.size(dim=-1))), 0
         )
         return MixtureSameFamily(categorical_experts, ind_expert_dist)
 
@@ -68,11 +71,11 @@ class MixtureOfExperts(nn.Module, Predictable):
         return (-log_prob).mean()  # NLL
 
     @torch.no_grad()
-    def next_state(self, states, actions, sample_n=1, deterministic=False):
+    def next_state(self, states, actions, deterministic=False):
         states = to_ts(states)
         actions = to_ts(actions)
         if not deterministic:
-            pred_delta = self.forward(states, actions).sample((sample_n,))
+            pred_delta = self.forward(states, actions).sample((states.shape[:-1], 1))
         else:
             pred_delta = self.forward(states, actions).mean
 
@@ -81,23 +84,16 @@ class MixtureOfExperts(nn.Module, Predictable):
             pred_delta * torch.sqrt(self.bb_out.running_var + self.bb_out.eps)
             + self.bb_out.running_mean
         )
-        return (states + pred_delta)[0, ...]
+        return states + pred_delta
 
-    def update_parameters(self, batch):
-        (
-            states,
-            next_states,
-            actions,
-            _,
-            _,
-        ) = batch.to_torch_batch()
+    def update(
+        self, states: torch.Tensor, actions: torch.Tensor, next_states: torch.Tensor
+    ):
         loss = self.loss(states, actions, next_states)
-
         self.optimizer.zero_grad()
         loss.backward()
         self.optimizer.step()
-
-        return loss.item()
+        return {"model_loss": loss.item()}
 
     def save(self, base_path, folder):
         path = base_path + folder + "/moe/"
