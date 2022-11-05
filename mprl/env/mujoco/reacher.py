b2c9294ac1c36a272f6ebc620c854c2e2e80b517
freiberg-roman <roman.sascha@googlemail.com>
2022-11-05 09:30:33 +0100

    fix envs for sac

--- mprl/env/mujoco/reacher.py
+++ mprl/env/mujoco/reacher.py
@@ -20,11 +20,12 @@ class ReacherEnv(MujocoEnv):
         observation = self._get_obs()
         self.current_steps += 1
         self._total_steps += 1
-        done = (
+        timeout = (
             self.time_out_after is not None
             and self.current_steps >= self.time_out_after
         )
-        return observation, reward, False, done, self.get_sim_state(), {}
+        failure = False
+        return observation, reward, failure, timeout
 
     def random_action(self):
         return np.random.uniform(-1, 1, (2,))
